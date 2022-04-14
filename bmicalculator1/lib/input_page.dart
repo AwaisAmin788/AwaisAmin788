8ce457d32772bc0f5538245257f3298514f0eba0
Awais Amin <98206865+AwaisAmin788@users.noreply.github.com>
2022-04-14 19:17:49 +0500

    use of Refactored code

--- bmicalculator1/lib/input_page.dart
+++ bmicalculator1/lib/input_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 
 
 class InputPage extends StatefulWidget {
@@ -19,6 +21,17 @@ class _InputPageState extends State<InputPage> {
             children: <Widget>[
               Expanded(child:RepeatContainerCode(
                 colors: Color(0xFF1D1E33),
+                cardWidget: Column(
+                  children: <widget>[
+                    Icon(FontAwesomeIcons.male,
+                      size: 80.0,,
+                    ),
+                    SizeBox(
+                      height:15.0,
+                    ),
+                  ],
+
+                ),
               ),),
               Expanded(child:RepeatContainerCode(
                 colors: Color(0xFF1D1E33),
@@ -50,12 +63,14 @@ class _InputPageState extends State<InputPage> {
 }
 
 class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({@required this.colors});
+  RepeatContainerCode({@required this.colors,this.cardWidget});
   final Color? colors;
+  final Widget cardWidget;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child: cardWidget,
       decoration: BoxDecoration(
         color: colors,
         borderRadius: BorderRadius.circular(10.0),
