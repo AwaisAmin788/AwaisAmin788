bf75d1d2b909433cf4efa5f4137ade2ed8554ad4
Awais Amin <98206865+AwaisAmin788@users.noreply.github.com>
2022-04-14 16:48:04 +0500

    UI of BMI

--- bmicalculator1/lib/input_page.dart
+++ bmicalculator1/lib/input_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -12,8 +13,56 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget> [
+          Expanded(child:Row(
+            children: <Widget>[
+              Expanded(child:Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),),
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),),
+
+            ],
+          ),),
+          Expanded(child:Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              color: Color(0xFF1D1E33),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ),),
+          Expanded(child: Row(
+          children: <Widget>[
+          Expanded(child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              color: Color(0xFF1D1E33),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ),),
+          Expanded(child:Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              color: Color(0xFF1D1E33),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ),
+          ),
+
+        ],
+      ),),
+
+        ],
       ),
 
     );
